f95d66ac43ef619774f0b902944cd73449be8ad9
Vee-ra <87364539+Vee-ra@users.noreply.github.com>
2022-02-24 19:23:46 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,64 @@
+var signupArr =[];
+var loginArr =[];
+
+function signUpValidation(){
+var fname = document.getElementById("n1").value;
+var lname = document.getElementById("n2").value;
+var signmobNum = document.getElementById("m1").value;
+var signupPwd = document.getElementById("p1").value;
+var birthField = document.getElementById("DOB_field").value;
+
+//code for paassword validation
+    if(fname == null || fname == ""){
+        alert("Name field can't be blank");
+        return false;
+    }
+    if(lname == null || lname == ""){
+        alert("Name field can't be blank");
+        return false;
+    }
+        if(signmobNum == null || signmobNum == ""){
+        alert("Enter valid mobile number");
+        return false;
+    }
+        if(signupPwd.length<6){
+        alert("Password must be atleaset 6 char long");
+        return false;
+    }
+        if(birthField == null || birthField ==""){
+        alert("Select your Birth DOB");
+        return false;
+    }
+        if(document.getElementById('Male').checked == false && document.getElementById('Female').checked == false 
+        && document.getElementById('Custom').checked == false ) { 
+        alert("Please select your gender");
+        return false;
+    }
+        var radio = document.querySelector('input[name=sex]:checked').value;
+        signupArr.push([fname,lname,signmobNum,signupPwd,birthField,radio]);//signup array
+        console.log(signupArr);
+        alert("User registered");
+     }   
+        function login(){
+            var logMobNum = document.getElementById("l1").value;
+            var logMobPwd = document.getElementById("l2").value;
+            
+            for(let i=0;i<signupArr.length;i++){
+                var innerArray=signupArr[i];
+               var phone= innerArray[2];
+               var pass=  innerArray[3];
+               console.log('phone :'+phone+'pass :'+pass);     
+            }
+            if(logMobNum == null && logMobNum == "" || logMobNum == null && logMobNum == ""){
+                alert("Please register");
+                }
+                if(phone === logMobNum && pass != logMobPwd){
+                alert("Incorrect password");
+                    }
+                    if(phone != logMobNum && pass != logMobPwd){
+                    alert("Please register");
+                        }
+                        if(phone === logMobNum && pass === logMobPwd){
+                        alert("success");
+                            }
+    }       
